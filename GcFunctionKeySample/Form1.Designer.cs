436a5be2094ace02ad497bde71f3657c5b4d1fce
Makoto Nishimura <mac.nishimura@gmail.com>
2016-09-13 01:23:36 +0900

    プロジェクト ファイルを追加します。

---
+++ GcFunctionKeySample/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace GcFunctionKeySample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.gcFunctionKey1 = new GrapeCity.Win.Bars.GcFunctionKey();
+            this.functionKeyButton1 = new GrapeCity.Win.Bars.FunctionKeyButton();
+            this.functionKeyButton2 = new GrapeCity.Win.Bars.FunctionKeyButton();
+            this.functionKeyButton3 = new GrapeCity.Win.Bars.FunctionKeyButton();
+            this.functionKeyButton4 = new GrapeCity.Win.Bars.FunctionKeyButton();
+            this.functionKeyButton5 = new GrapeCity.Win.Bars.FunctionKeyButton();
+            this.functionKeyButton6 = new GrapeCity.Win.Bars.FunctionKeyButton();
+            this.functionKeyButton7 = new GrapeCity.Win.Bars.FunctionKeyButton();
+            this.functionKeyButton8 = new GrapeCity.Win.Bars.FunctionKeyButton();
+            this.functionKeyButton9 = new GrapeCity.Win.Bars.FunctionKeyButton();
+            this.functionKeyButton10 = new GrapeCity.Win.Bars.FunctionKeyButton();
+            this.functionKeyButton11 = new GrapeCity.Win.Bars.FunctionKeyButton();
+            this.functionKeyButton12 = new GrapeCity.Win.Bars.FunctionKeyButton();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "押されたキー";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(117, 29);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 12);
+            this.label2.TabIndex = 2;
+            // 
+            // gcFunctionKey1
+            // 
+            this.gcFunctionKey1.FunctionKeyButtons.AddRange(new GrapeCity.Win.Bars.FunctionKeyButton[] {
+            this.functionKeyButton1,
+            this.functionKeyButton2,
+            this.functionKeyButton3,
+            this.functionKeyButton4,
+            this.functionKeyButton5,
+            this.functionKeyButton6,
+            this.functionKeyButton7,
+            this.functionKeyButton8,
+            this.functionKeyButton9,
+            this.functionKeyButton10,
+            this.functionKeyButton11,
+            this.functionKeyButton12});
+            this.gcFunctionKey1.Location = new System.Drawing.Point(0, 339);
+            this.gcFunctionKey1.Name = "gcFunctionKey1";
+            this.gcFunctionKey1.Size = new System.Drawing.Size(628, 25);
+            this.gcFunctionKey1.TabIndex = 3;
+            this.gcFunctionKey1.Text = "gcFunctionKey1";
+            // 
+            // functionKeyButton1
+            // 
+            this.functionKeyButton1.FunctionKey = System.Windows.Forms.Keys.F1;
+            this.functionKeyButton1.Name = "functionKeyButton1";
+            this.functionKeyButton1.Size = new System.Drawing.Size(84, 22);
+            this.functionKeyButton1.Text = "Function1";
+            // 
+            // functionKeyButton2
+            // 
+            this.functionKeyButton2.FunctionKey = System.Windows.Forms.Keys.F2;
+            this.functionKeyButton2.Name = "functionKeyButton2";
+            this.functionKeyButton2.Size = new System.Drawing.Size(84, 22);
+            this.functionKeyButton2.Text = "Function2";
+            // 
+            // functionKeyButton3
+            // 
+            this.functionKeyButton3.FunctionKey = System.Windows.Forms.Keys.F3;
+            this.functionKeyButton3.Name = "functionKeyButton3";
+            this.functionKeyButton3.Size = new System.Drawing.Size(84, 22);
+            this.functionKeyButton3.Text = "Function3";
+            // 
+            // functionKeyButton4
+            // 
+            this.functionKeyButton4.FunctionKey = System.Windows.Forms.Keys.F4;
+            this.functionKeyButton4.Name = "functionKeyButton4";
+            this.functionKeyButton4.Size = new System.Drawing.Size(84, 22);
+            this.functionKeyButton4.Text = "Function4";
+            // 
+            // functionKeyButton5
+            // 
+            this.functionKeyButton5.FunctionKey = System.Windows.Forms.Keys.F5;
+            this.functionKeyButton5.Name = "functionKeyButton5";
+            this.functionKeyButton5.Size = new System.Drawing.Size(84, 22);
+            this.functionKeyButton5.Text = "Function5";
+            // 
+            // functionKeyButton6
+            // 
+            this.functionKeyButton6.FunctionKey = System.Windows.Forms.Keys.F6;
+            this.functionKeyButton6.Name = "functionKeyButton6";
+            this.functionKeyButton6.Size = new System.Drawing.Size(84, 22);
+            this.functionKeyButton6.Text = "Function6";
+            // 
+            // functionKeyButton7
+            // 
+            this.functionKeyButton7.FunctionKey = System.Windows.Forms.Keys.F7;
+            this.functionKeyButton7.Name = "functionKeyButton7";
+            this.functionKeyButton7.Size = new System.Drawing.Size(84, 22);
+            this.functionKeyButton7.Text = "Function7";
+            // 
+            // functionKeyButton8
+            // 
+            this.functionKeyButton8.FunctionKey = System.Windows.Forms.Keys.F8;
+            this.functionKeyButton8.Name = "functionKeyButton8";
+            this.functionKeyButton8.Size = new System.Drawing.Size(84, 22);
+            this.functionKeyButton8.Text = "Function8";
+            // 
+            // functionKeyButton9
+            // 
+            this.functionKeyButton9.FunctionKey = System.Windows.Forms.Keys.F9;
+            this.functionKeyButton9.Name = "functionKeyButton9";
+            this.functionKeyButton9.Size = new System.Drawing.Size(84, 20);
+            this.functionKeyButton9.Text = "Function9";
+            // 
+            // functionKeyButton10
+            // 
+            this.functionKeyButton10.FunctionKey = System.Windows.Forms.Keys.F10;
+            this.functionKeyButton10.Name = "functionKeyButton10";
+            this.functionKeyButton10.Size = new System.Drawing.Size(96, 20);
+            this.functionKeyButton10.Text = "Function10";
+            // 
+            // functionKeyButton11
+            // 
+            this.functionKeyButton11.FunctionKey = System.Windows.Forms.Keys.F11;
+            this.functionKeyButton11.Name = "functionKeyButton11";
+            this.functionKeyButton11.Size = new System.Drawing.Size(96, 20);
+            this.functionKeyButton11.Text = "Function11";
+            // 
+            // functionKeyButton12
+            // 
+            this.functionKeyButton12.FunctionKey = System.Windows.Forms.Keys.F12;
+            this.functionKeyButton12.Name = "functionKeyButton12";
+            this.functionKeyButton12.Size = new System.Drawing.Size(96, 20);
+            this.functionKeyButton12.Text = "Function12";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(628, 364);
+            this.Controls.Add(this.gcFunctionKey1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private GrapeCity.Win.Bars.GcFunctionKey gcFunctionKey1;
+        private GrapeCity.Win.Bars.FunctionKeyButton functionKeyButton1;
+        private GrapeCity.Win.Bars.FunctionKeyButton functionKeyButton2;
+        private GrapeCity.Win.Bars.FunctionKeyButton functionKeyButton3;
+        private GrapeCity.Win.Bars.FunctionKeyButton functionKeyButton4;
+        private GrapeCity.Win.Bars.FunctionKeyButton functionKeyButton5;
+        private GrapeCity.Win.Bars.FunctionKeyButton functionKeyButton6;
+        private GrapeCity.Win.Bars.FunctionKeyButton functionKeyButton7;
+        private GrapeCity.Win.Bars.FunctionKeyButton functionKeyButton8;
+        private GrapeCity.Win.Bars.FunctionKeyButton functionKeyButton9;
+        private GrapeCity.Win.Bars.FunctionKeyButton functionKeyButton10;
+        private GrapeCity.Win.Bars.FunctionKeyButton functionKeyButton11;
+        private GrapeCity.Win.Bars.FunctionKeyButton functionKeyButton12;
+    }
+}
+
---
+++ GcFunctionKeySample/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GcFunctionKeySample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void gcFunctionKey1_FunctionKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.F1)
+            {
+                MessageBox.Show("F1キーが押されました");
+            }
+            this.label2.Text = e.KeyCode.ToString();
+        }
+    }
+}
